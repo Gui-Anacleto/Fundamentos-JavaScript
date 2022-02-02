d677f24bc551fd8ba3aaf5209e61ab9e080fbe29
Paquista0 <guilherme_fa@outlook.com>
2022-02-02 16:56:19 -0300

    first commit

--- type-string.js
+++ type-string.js
@@ -48,3 +48,11 @@ console.log(cidade1 === inputMinusculo); // true
 
 const senha1 = "minhaSenha123"
 console.log(senha1.length) // 13 caracteres
+
+//tipos
+const minhaVar = 1;
+const MinhaVar = "texto";
+const minhavar = "3";
+const MINHAVAR = 2;
+
+console.log(minhaVar, MinhaVar, minhavar, MINHAVAR)
