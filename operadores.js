793716fc45122cc1896ca334955e4a48fbffa52d
Paquista0 <guilherme_fa@outlook.com>
2022-02-07 11:27:39 -0300

    Update operadores.js

--- operadores.js
+++ operadores.js
@@ -21,3 +21,17 @@ String()
 ||: Operador “ou”, retorna true caso uma condição seja válida;
 &&: Operador “e”, retorna true somente se todas as condições forem válidas;
 != e !==: Operadores “não igual” e “estritamente não igual”, utilizados para comparação, da mesma forma que == e === retornam true ou false. */
+
+//Operador ternário (comparação em apenas uma linha)
+
+const idadeMin = 18;
+const idadeCli = 16;
+
+if (idadeCli >= idadeMin){
+    console.log('Cerveja')
+}else{
+    console.log('Suco')
+}
+
+
+console.log(idadeCli >= idadeMin ? "Cerveja" : "Suco")
