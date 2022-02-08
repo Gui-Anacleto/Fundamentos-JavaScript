497c10fabb0dc8c1c3cd874ca28c895d49adea19
Paquista0 <guilherme_fa@outlook.com>
2022-02-08 09:15:58 -0300

    commitado

--- operadores.js
+++ operadores.js
@@ -35,3 +35,12 @@ if (idadeCli >= idadeMin){
 
 
 console.log(idadeCli >= idadeMin ? "Cerveja" : "Suco")
+
+
+/* const nome = "Leo";
+const idade = 23;
+const bebidaMaior = "cerveja";
+const bebidaMenor = "suco";
+
+const pedido = `${nome} diz: "por favor, quero beber ${idade >= 18 ? bebidaMaior : bebidaMenor}"`
+console.log(pedido) */
---
+++ template-strings.js
@@ -0,0 +1,9 @@
+const nome = "Guilherme";
+const idade = 2021-2000;
+const city = "Ourinhos";
+
+/* const apresentacao = "meu nome é " + nome + " ,minha idade é " + idade + "e nasci na cidade de " + city; 
+console.log(apresentacao) */
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${city}`
+console.log(apresentacao)
